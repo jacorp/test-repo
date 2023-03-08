e3d41ff9dda7e0bc5ab87942cf3d4604272d54d6
User <kan3496@gmail>
2023-03-08 09:30:22 +0900

    part 구분

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -5,6 +5,12 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Where are you from?");
+		
+		/* Part of initialization */
+		
+		/* Part of running */
+		
+		/* Part of closing */
 	}
 
 }
